2eb4d9fb5b46396c4eb84d257d67c56db6534899
doliroj929 <doliroj929@g.educaand.es>
2026-04-27 13:15:59 +0200

    js transfomacion

---
+++ transformacion.js
@@ -0,0 +1,15 @@
+console.log("ddd")
+const xmlRecetas = fetch("./recetas.xml");
+const xmlRecetasString = xmlRecetas.text;
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(xmlRecetasString, "application/xml");
+
+function xmlToJson(xml) {
+    const obj = {};
+    for (let node of xml.children) {
+        obj[node.nodeName] = node.textContent;
+    }
+    return obj;
+}
+
+console.log(xmlToJson(xmlDoc.documentElement));
